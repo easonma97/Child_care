aae321e48e6555dc9ef93501163d8df0139947de
Eason Ma <a705193483@gmail.com>
2023-10-13 23:34:37 +1030

    update UI logic

--- kai-project/src/components/SurveyForm.jsx
+++ kai-project/src/components/SurveyForm.jsx
@@ -1,41 +1,81 @@
-import React, { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+// Default V2 theme
 import 'survey-core/defaultV2.min.css';
+// Modern theme
+// import 'survey-core/modern.min.css';
 import { Model, EmailValidator } from 'survey-core';
 import { Survey } from 'survey-react-ui';
+// import surveyJson
 import surveyJson from '../models/surveyJson';
+import axios from "axios";
 
-const SURVEY_ID = 1;
+function SurveyForm() {
+    const [showAnswer, setShowAnswer] = useState(false);
+    const [answer, setAnswer] = useState("");
+    const survey = new Model(surveyJson);
+    const email = survey.getQuestionByName("email-address");
+    email.validators.push(new EmailValidator());
+    
+    const alertResults = useCallback(async (sender) => {
+        // const results = JSON.stringify(sender.data);
+        // alert(results);
+        // Pass the survey results to OpenAI API for chat completion
+        try{
+          const openAIResponse = await sendSurveyDataToOpenAI(sender.data);
+          console.log(openAIResponse);
+          if(openAIResponse){
+            setShowAnswer(true);
+            setAnswer(openAIResponse);
+          }
+        } catch (error) {
+          console.error("Error in alertResults:", error);
+        }
+        // alert(results);
+      }, []);
 
-const SurveyForm = () => {
-  const survey = new Model(surveyJson);
-  const email = survey.getQuestionByName('email-address');
-  email.validators.push(new EmailValidator());
+    survey.onComplete.add(alertResults);
 
-  const surveyComplete = useCallback((sender) => {
-    saveSurveyResults('https://your-web-service.com/' + SURVEY_ID, sender.data);
-  }, []);
+    const sendSurveyDataToOpenAI = async (surveyData) => {
+      try {
+        const response = await axios.post(
+          "https://api.openai.com/v1/engines/text-davinci-003/completions",
+          {
+            prompt: JSON.stringify(surveyData),
+            max_tokens: 2049, // Adjust the number of tokens as needed
+          },
+          {
+            headers: {
+              Authorization: "Bearer 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",
+            },
+          }
+        );
+        
+        const openAIResponse = response.data.choices[0].text;
+        // Do something with the response from OpenAI, such as updating your UI
+        console.log("Full API Response:", response.data); // Print full response
+        console.log("OpenAI response:", openAIResponse);
+        alert(openAIResponse);
+        return openAIResponse;
+      } catch (error) {
+        console.error("Error sending data to OpenAI:", error);
+        console.log("Full Error Response:", error.response.data); // Print full error response
+      }
+    };  
 
-  const alertResults = useCallback((sender) => {
-    const results = JSON.stringify(sender.data);
-    alert(results);
-  }, []);
-
-  survey.onComplete.add(alertResults);
-
-  return <Survey model={survey} />;
-};
-
-const saveSurveyResults = (url, json) => {
-  const request = new XMLHttpRequest();
-  request.open('POST', url);
-  request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-  request.addEventListener('load', () => {
-    // Handle "load"
-  });
-  request.addEventListener('error', () => {
-    // Handle "error"
-  });
-  request.send(JSON.stringify(json));
-};
-
+    return (<div>{showAnswer? (<div>{answer}</div>) : (<Survey model={survey} />)}</div>);
+}
+/*
+function saveSurveyResults(url, json) {
+    const request = new XMLHttpRequest();
+    request.open('POST', url);
+    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    request.addEventListener('load', () => {
+      // Handle "load"
+    });
+    request.addEventListener('error', () => {
+      // Handle "error"
+    });
+    request.send(JSON.stringify(json));
+}
+*/
 export default SurveyForm;
